85498d74dd46f2f96c4b9221c9dc47958dc7f981
Nick Riebeek <nick.riebeek@gmail.com>
2018-05-15 16:45:41 -0400

    added parsing of remaining fields

--- lib/twitter_feed/parser.ex
+++ lib/twitter_feed/parser.ex
@@ -13,8 +13,19 @@ defmodule TwitterFeed.Parser do
 
   ##################### PRIVATE FUNCTIONS #####################
   defp parse_tweet(tweet_html) do
+    user_id = parse_user_id(tweet_html)
+    is_retweet = parse_is_retweet(tweet_html)
+
     %Tweet {
-      display_name: parse_display_name(tweet_html)
+      handle_id: parse_handle_id(is_retweet, user_id, tweet_html),
+      user_id: user_id,
+      user_name: parse_user_name(tweet_html),
+      display_name: parse_display_name(tweet_html),
+      tweet_id: parse_tweet_id(tweet_html),
+      timestamp: parse_timestamp(tweet_html),
+      text_summary: parse_text(tweet_html) |> truncate(),
+      image_url: parse_image(tweet_html),
+      retweet: is_retweet
     }
   end
 
@@ -23,5 +34,75 @@ defmodule TwitterFeed.Parser do
     tweet_html
     |> Floki.attribute("data-name")
     |> hd()
+  end
+
+  defp parse_user_id(tweet_html) do
+    tweet_html
+    |> Floki.attribute("data-user-id")
+    |> hd()
+    |> String.to_integer()
+  end
+
+  defp parse_user_name(tweet_html) do
+    tweet_html
+    |> Floki.attribute("data-screen-name")
+    |> hd()
+  end
+
+  defp parse_tweet_id(tweet_html) do
+    tweet_html
+    |> Floki.attribute("data-tweet-id")
+    |> hd()
+    |> String.to_integer()
+  end
+
+  defp parse_timestamp(tweet_html) do
+    tweet_html
+    |> Floki.find("._timestamp")
+    |> Floki.attribute("data-time-ms")
+    |> hd()
+    |> String.to_integer()
+    |> DateTime.from_unix!(:millisecond)
+    |> DateTime.to_string()
+  end
+
+  defp parse_text(tweet_html) do
+    tweet_html
+    |> Floki.find(".tweet-text")
+    |> Floki.text()
+    |> String.trim()
+  end
+
+  defp parse_image(tweet_html) do
+    tweet_html
+    |> Floki.find(".AdaptiveMedia-photoContainer")
+    |> Floki.attribute("data-image-url")
+    |> Floki.text()
+  end
+
+  defp parse_is_retweet(tweet_html) do
+    tweet_html
+    |> Floki.attribute("data-retweeter")
+    |> Enum.count() == 1
+  end
+
+  defp parse_handle_id(false, user_id, _tweet_html) do
+    user_id
+  end
+
+  defp parse_handle_id(true, _user_id, tweet_html) do
+    tweet_html
+    |> Floki.find(".js-retweet-text > a")
+    |> Floki.attribute("data-user-id")
+    |> hd()
+    |> String.to_integer()
+  end
+
+  defp truncate(text) do
+    if (String.length(text)) > 30 do
+      String.slice(text, 0, 30) <> "..."
+    else
+      text
+    end
   end
 end
--- test/twitter_feed/parser_test.exs
+++ test/twitter_feed/parser_test.exs
@@ -8,4 +8,80 @@ defmodule TwitterFeed.ParserTest do
 
     assert Parser.parse_display_name(html_snippet) == "lola"
   end
+
+  test "parsing of user_id" do
+    html_snippet = "<div class=\"tweet\" data-user-id=\"2\"></div>"
+
+    assert Parser.parse_user_id(html_snippet) == 2
+  end
+
+  test "parsing of user_name" do
+    html_snippet = "<div class=\"tweet\" data-screen-name=\"SomeUserName\"></div>"
+
+    assert Parser.parse_user_name(html_snippet) == "SomeUserName"
+  end
+
+  test "parsing of tweet_id" do
+    html_snippet = "<div class=\"tweet\" data-tweet-id=\"1\"></div>"
+
+    assert Parser.parse_tweet_id(html_snippet) == 1
+  end
+
+  test "parsing of timestamp" do
+    html_snippet = "<span class=\"_timestamp\" data-time-ms=\"1519339506000\"</span>"
+
+    assert Parser.parse_timestamp(html_snippet) == "2018-02-22 22:45:06.000Z"
+  end
+
+  test "parsing of tweet_text" do
+    html_snippet = "<p class=\"tweet-text\">some text</p>"
+
+    assert Parser.parse_text(html_snippet) == "some text"
+  end
+
+  test "parsing of tweet_image" do
+    html_snippet = """
+      <div class=\"AdaptiveMedia-photoContainer\"
+           data-image-url=\"https://pbs.twimg.com/media/123.jpg\">
+      </div>
+      """
+
+    assert Parser.parse_image(html_snippet) == "https://pbs.twimg.com/media/123.jpg"
+  end
+
+  test "parsing of retweeter when it is a retweet" do
+    html_snippet = "<div class=\"tweet\" data-retweeter=\"TorontoComms\"></div>"
+
+    assert Parser.parse_is_retweet(html_snippet) == true
+  end
+
+  test "parsing of retweeter when it is not a retweet" do
+    html_snippet = "<div class=\"tweet\"></div>"
+
+    assert Parser.parse_is_retweet(html_snippet) == false
+  end
+
+  test "parsing of handle_id when it is not a retweet" do
+    assert Parser.parse_handle_id(false, 123, "some html") == 123
+  end
+
+  test "parsing of handle_id when it is a retweet" do
+    html_snippet = """
+    <span class=\"js-retweet-text\">
+     <a data-user-id=\"19377913\"><b>City of Toronto</b></a> Retweeted
+    </span>
+    """
+
+    assert Parser.parse_handle_id(true, 123, html_snippet) == 19377913
+  end
+
+  test "truncation of text that does not exceed 30 chars" do
+    assert Parser.truncate("some text") == "some text"
+  end
+
+  test "truncation of text over 30 chars is truncated" do
+    text = "This is some text that is 31 ch"
+
+    assert Parser.truncate(text) == "This is some text that is 31 c..."
+  end
 end
