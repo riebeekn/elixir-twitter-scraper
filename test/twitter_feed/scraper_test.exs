10f4e7f8f667f1650210536965be02dbc8592197
Nick Riebeek <nick.riebeek@gmail.com>
2018-06-05 16:43:06 -0400

    fixed tests

--- test/twitter_feed/scraper_test.exs
+++ test/twitter_feed/scraper_test.exs
@@ -4,13 +4,13 @@ defmodule TwitterFeed.ScraperTest do
   alias TwitterFeed.{ Scraper }
 
   test "scraping on non-existant handle will return 404" do
-    {:error, reason} = Scraper.scrape(:non_existant_handle, 1, 0)
+    {:error, reason} = Scraper.scrape(:non_existant_handle, 0)
 
     assert reason =~ "404 error, that handle does not exist"
   end
 
   test "scraping on valid handle will return some body content" do
-    body = Scraper.scrape(:any_handle, 1, 0)
+    body = Scraper.scrape(:any_handle, 0)
 
     assert body =~ "This handle looks good!"
   end
