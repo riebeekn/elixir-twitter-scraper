fe73021f1dacb0589e59d2914693d6d076783ef3
Nick Riebeek <nick.riebeek@gmail.com>
2018-07-17 17:13:13 -0400

    added some additional error checking and 302 redirect handling

--- lib/twitter_feed/scraper.ex
+++ lib/twitter_feed/scraper.ex
@@ -5,24 +5,40 @@ defmodule TwitterFeed.Scraper do
 
   alias TwitterFeed.Parser
 
+  def scrape(_handle, start_after_tweet) when start_after_tweet < 0 do
+    {:error, "invalid start_after_tweet argument, can't be < 0"}
+  end
+
   def scrape(handle, 0) do
     case @twitter_api.get_home_page(handle) do
       {:ok, %{status_code: 200, body: body}} ->
         body
         |> Parser.parse_tweets(:html)
+      {:ok, %{status_code: 302, headers: headers}} ->
+        headers
+        |> return_302()
       {:ok, %{status_code: 404}} ->
         return_404()
     end
   end
 
-  def scrape(handle, start_after_tweet) do
+  def scrape(handle, start_after_tweet) when start_after_tweet > 0 do
     case @twitter_api.get_tweets(handle, start_after_tweet) do
       {:ok, %{status_code: 200, body: body}} ->
         body
         |> Parser.parse_tweets(:json)
+      {:ok, %{status_code: 302, headers: headers}} ->
+        headers
+        |> return_302()
       {:ok, %{status_code: 404}} ->
         return_404()
     end
+  end
+
+  defp return_302(headers) do
+    header_map = Enum.into headers, %{}
+    url = header_map["location"]
+    {:redirect, "302 redirect received, redirect address: #{url}"}
   end
 
   defp return_404 do
--- lib/twitter_feed/twitter_api/http_client.ex
+++ lib/twitter_feed/twitter_api/http_client.ex
@@ -7,11 +7,20 @@ defmodule TwitterFeed.TwitterApi.HttpClient do
 
   def get_home_page(handle) do
     UrlBuilder.build_html_url(handle)
-    |> HTTPoison.get()
+    |> HTTPoison.get(get_headers())
   end
 
   def get_tweets(handle, last_tweet_retrieved) do
     UrlBuilder.build_json_url(handle, last_tweet_retrieved)
-    |> HTTPoison.get()
+    |> HTTPoison.get(get_headers())
+  end
+
+  defp get_headers do
+    [
+      {
+        "user-agent",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
+      }
+    ]
   end
 end
